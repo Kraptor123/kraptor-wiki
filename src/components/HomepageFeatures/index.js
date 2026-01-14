3d2806b500c7b89cf89b848a53125ca33654ffcb
kraptor123 <kraptor121@gmail.com>
2026-01-14 06:21:42 +0300

    giriş sayfası

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -6,6 +6,7 @@ const FeatureList = [
     {
         title: 'Cs-Kraptor',
         imgSrc: './img/kraptorlogo.png',
+        link: 'https://github.com/Kraptor123/cs-kraptor',
         description: (
             <>
                 Cs-Kraptor sadece Türk kullanıcıları ilgilendiren eklentileri barındıran ana repodur.
@@ -15,28 +16,22 @@ const FeatureList = [
     {
         title: 'Cs-Karma',
         imgSrc: './img/cskarma.png',
+        link: 'https://github.com/Kraptor123/Cs-Karma',
         description: (
             <>
                 Cs-Karma eklentileri türlerine takılmadan karma şekilde yüklenen birden çok ülkeye destek veren yan repodur.
             </>
         ),
     },
-    {
-        title: 'Gizli Keyif',
-        imgSrc: './img/gizlikeyifogo.png',
-        description: (
-            <>
-                Bunu boşverin.
-            </>
-        ),
-    },
 ];
 
-function Feature({imgSrc, title, description}) {
+function Feature({imgSrc, title, description, link}) {
     return (
-        <div className={clsx('col col--4')}>
+        <div className={clsx('col col--6')}>
             <div className="text--center">
-                <img src={imgSrc} className={styles.featureSvg} alt={title} />
+                <a href={link} target="_blank" rel="noopener noreferrer">
+                    <img src={imgSrc} className={styles.featureSvg} alt={title} />
+                </a>
             </div>
             <div className="text--center padding-horiz--md">
                 <Heading as="h3">{title}</Heading>
@@ -47,15 +42,15 @@ function Feature({imgSrc, title, description}) {
 }
 
 export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row justify-content--center">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
