b294c56d96f5587c53f96bd85042c36cd237c5cb
kraptor123 <kraptor121@gmail.com>
2026-01-14 05:26:56 +0300

    yonlendirme

--- static/tarayici/script.js
+++ static/tarayici/script.js
@@ -98,7 +98,7 @@ class CloudStreamBrowser {
 
         const backBtn = document.getElementById('backButton');
         if (backBtn) backBtn.addEventListener('click', () => {
-            window.location.href = '/kraptor-wiki/docs/eklenti-tarayici';
+            window.location.href = '/kraptor-wiki/docs/category/eklenti-tarayıcı';
         });
 
         const headerLogo = document.querySelector('.header-logo');
